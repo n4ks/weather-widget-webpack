74ad6f10453ca8143922f71f9498deb4a4ea1eff
Mazur Dmitry <innownedagain@gmail.com>
2022-09-01 20:30:43 +0300

    formatted sunset and sunrise time

--- src/utils/dates/dates.ts
+++ src/utils/dates/dates.ts
@@ -1,7 +1,14 @@
-﻿const getTimeFromUnixTimestamp = (timestamp: number): string => {
+﻿const addLeadingZeroToTime = (time: number) => {
+  return time < 10 ? '0' + time : time;
+};
+
+const getTimeFromUnixTimestamp = (timestamp: number): string => {
   const date = new Date(timestamp * 1000);
 
-  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+  const formattedTime = time.map(addLeadingZeroToTime);
+
+  return formattedTime.join(':');
 };
 
 export { getTimeFromUnixTimestamp };
